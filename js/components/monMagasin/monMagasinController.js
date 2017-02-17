73f65f152cfdf116f1eeeae80eeccaaa2ad7368e
Elsa Delgado <elsadd@hotmail.fr>
2017-02-17 13:47:51 +0100

    marqueur

--- js/components/monMagasin/monMagasinController.js
+++ js/components/monMagasin/monMagasinController.js
@@ -7,10 +7,16 @@ angular.module('monMagasin').controller('monMagasinController', ["$http", "$scop
 			zoom: 14,
 			zoomControl: false
 		});
+
+		L.marker([40.741895,-73.989308]).addTo(map);
+		L.marker([40.741890,-73.989400]).addTo(map);
+		
 		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
 		}).addTo(map);
 
 
+ 
+		// 
 
 	}])
